f96389a07f5ef03b591293a12da28ce41e21a988
NRostova <mzyrianova14@gmail.com>
2020-11-18 13:52:15 -0800

    Rename header classes according to BEM methodology

--- header.php
+++ header.php
@@ -4,25 +4,20 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <?php wp_head()?>
-       
         <script src="https://kit.fontawesome.com/1ec81f68fa.js"></script>
         <title>Masha Zyryanova</title>    
     </head>
-    <body <?php body_class('site'); ?>>
-      <header class="site__header header sticky" >
-		  
-		  
-        <div class="header_desktop header__content narrow content" >
-            <div class="logo header__logo">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-              			<h1 class = "logo__text">MYZ</h1>
-				</a>
-            </div>
-            <nav class="header__nav nav">
-              
-                <?php
-
-                if(has_nav_menu('primary')){
+    <body <?php body_class('site'); ?> >
+      <header class="site__header site__header--sticky header">
+        <div class="header__desktop header__content narrow content" >
+          <div class="logo header__logo">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+              <h1 class = "logo__text">MYZ</h1>
+            </a>
+          </div>
+          <nav class="header__nav nav">
+            <?php
+              if(has_nav_menu('primary')){
                   wp_nav_menu([
                     'theme_location'  => 'primary',
                     'container'       => 'false',
@@ -30,28 +25,26 @@
                     'fallback_cb'     => false,
                     'depth'           => 2
                   ]);
-                }
-                ?>
-            </nav>     
+              }
+            ?>
+          </nav>     
         </div> 
-		  
-        <div class="header_mobile mobile" >  
-          <div class="logo header__logo">
+        <!-- End desktop header -->
+   
+        <div class="header__mobile mobile" >  
+          <div class="header__logo logo">
             	 <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
               			<h1 class = "logo__text">MYZ</h1>
-				</a>
+				        </a>
           </div>
-          <div class="mobile__menu mmm">
+          <div class="mobile__menu">
               <a class="menu-toggle">
                 <i  class="fas fa-bars fa-2x"></i>
               </a>
           </div>
-          <!-- side nav -->
         </div>
         <!-- End Mobile Header  -->
-      </header> 
-      <!-- End Header -->
-		
+      </header>
 		
       <div id="sidenav" class="sidenav">
           <a href="javascript:void(0)"  class="closebtn">&times;</a>
